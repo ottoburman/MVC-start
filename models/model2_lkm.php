1e495102cc7e794ec8e082aa18510fdf4a779402
Otto Burman <31030995+ottoburman@users.noreply.github.com>
2018-12-06 16:24:45 +0200

    model2_chat.php
    Tarkistaa rivienäärän - kommenttien korjailua

--- models/model2_lkm.php
+++ models/model2_lkm.php
@@ -2,7 +2,7 @@
 		# Tutkitaan viestien määrä lkm-muuttujasta
 			if ($lkm > 6) /* jos suurempi kuin 50 niin */
 			{
-			file_put_contents("datat/data1_chat.txt", "*Vain viisi(kymmentä) riviä ja nollataan... Aloitus... \n ");
-			/* koska file_put_contents ei lopu appen -määritykseen niin kaikki tiedot korvautuvat. */
+			file_put_contents("datat/data1_chat.txt", "* Ehto ($lkm>6) riviä ja nollataan... Aloitus... \n ");
+			/* koska file_put_contents ei lopu tässä append -määritykseen niin kaikki tiedot korvautuvat. */
 			}
 ?>
