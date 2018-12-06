53eed95109327d6fbb6d30e212005781164de73d
Otto Burman <31030995+ottoburman@users.noreply.github.com>
2018-12-06 16:21:21 +0200

    model1_chat.php
    lisää kommentteja

--- models/model1_chat.php
+++ models/model1_chat.php
@@ -2,15 +2,17 @@
 class Malli {
     private $tiedosto = "datat/data1_chat.txt";
     public function viestit() {
+    // luetaan data1-tiedostosta jos on olemassa... 
         if (file_exists($this->tiedosto)) 
 		{ 	return file($this->tiedosto); } 
 		else 
 		{	return array();   }
-    }
+    }		
     public function lisaa($viesti, $nimim) {
-		// funktio lähetys kutsuu funktiota lisaa
-        $viesti = date("H:i:s: ") . $nimim . ": ". $viesti;
-        file_put_contents($this->tiedosto, "{$viesti}\n", FILE_APPEND);
+		// funktio lisaa tietoja ja kirjoittaa viesti-muuttujaan tietoja
+       $viesti = date("H:i:s: ") . $nimim . ": ". $viesti;
+	    	// tiedosto-muuttujan osoittamaan tiedostoon
+         file_put_contents($this->tiedosto, "{$viesti}\n", FILE_APPEND);
     }
 }
 ?>
