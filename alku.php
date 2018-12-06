cc5086908b081864d9c52118733732623c3309f0
Otto Burman <ottoburman@gmail.com>
2018-12-06 16:32:17 +0200

    Update alku.php

--- alku.php
+++ alku.php
@@ -1,6 +1,6 @@
 <?php
 include("models/model1_chat.php");// haetaan model1-malli
-include("control_chat.php");
+include("control_chat.php");// haetaan tähän kontrolleri
 $ohjain = new Ohjain();
 if (isset($_GET["toiminto"])) {
     $toiminto = $_GET["toiminto"];
